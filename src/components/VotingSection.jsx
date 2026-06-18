61d0e20df96cac98f9e63602f9f8fd6b548cd5b0
Daniela Bastias <bastiasdany.84@gmail.com>
2026-06-18 12:19:49 -0300

    Editando links de votos

--- src/components/VotingSection.jsx
+++ src/components/VotingSection.jsx
@@ -25,33 +25,6 @@ const VotingSection = () => {
       </motion.p>
 
       <div className="voting-banners">
-        {/* TopG */}
-        <motion.a
-          href="https://topg.org/lineage2-private-servers/server-671210#vote"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="vote-img-link"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <img src="https://topg.org/topg.gif" alt="Vote on TopG" />
-        </motion.a>
-
-        {/* Hopzone */}
-        <motion.a
-          href="https://l2.hopzone.net/site/vote/106806/1"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="vote-img-link"
-          whileHover={{ scale: 1.1 }}
-          whileTap={{ scale: 0.95 }}
-        >
-          <img
-            src="https://l2.hopzone.net/assets/img/banners/vote_banners/banner_2.png"
-            alt="Vote on Hopzone"
-          />
-        </motion.a>
-
         {/* L2Brasil */}
         <motion.a
           href="https://top.l2jbrasil.com/index.php?a=in&u=ciber-naka"
